aed33e58e7ad9cb1f7c3647995dc25922079a494
Akmal Izuddin <izuddin0407@gmail.com>
2022-06-03 10:16:02 +0800

    minor additions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const messages = [{
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date().toString()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date().toString()
+  }];
+
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
